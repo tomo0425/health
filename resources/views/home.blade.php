c7ac67b51ee668794a5bc346b9b45fa75a449697
tomo0425 <onishi.tomoki@g.wakayama-u.jp>
2023-08-19 07:55:00 +0000

    始めのページ

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body style="background-position: center center; background-repeat: no-repeat; background-size: cover; background-image:url(https://thumb.ac-illust.com/47/4719bf17042721dd7f5dd07c5086eb31_t.jpeg); ">
+        <div class="container " style="margin-top:150px;">
+            <div class=" py-6 sm:py-8 lg:py-12">
+              <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+                <div class="rounded-lg bg-gray-100 px-4 py-6 md:py-8 lg:py-12">
+                  <p class="mb-2 text-center font-semibold text-indigo-500 md:mb-3 lg:text-lg"></p>
+            
+                  <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Healthee!</h2>
+            
+                  <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">コンビニの商品で健康を促進するアプリです。</p>
+                </div>
+              </div>
+            </div>
+            <div class="row" style="margin-top: 50px;">
+                <div class="col-md-6 text-right" style="padding-right:50px;">
+                    <a href="/login" class="btn btn-primary" style="font-size: 24px; padding: 12px 24px;">ログイン</a>
+                </div>
+                <div class="col-md-6 text-left" style="padding-left:50px;">
+                    <a href="/register" class="btn btn-primary" style="font-size: 24px; padding: 12px 24px;">登録する</a>
+                </div>
+            </div>
+        </div>    
+        
+        
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+    </body>
+</html>
